01628381b860f7a380d58880a2c61c4d328b8235
Bishnupkl <bishnup212@gmail.com>
2020-09-07 12:14:33 +0545

    database connection establish

---
+++ dbconfig.php
@@ -0,0 +1,14 @@
+<?php 
+
+ $host='localhost';
+ $db='esewaphp';
+ $user='20nu';
+ $pass='20nu';
+
+ $conn=new mysqli($host,$db,$user,$pass);
+ if($conn->connect_error){
+ 	echo "failed to connect mySQL".$conn->connect_error;
+ 	exit;
+ }
+
+ ?>
