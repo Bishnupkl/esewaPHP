e26fd19caaa751121c67048cc5932ed9e622a0fe
Bishnupkl <bishnup212@gmail.com>
2020-09-07 21:46:21 +0545

    integrating esewa started

--- checkout.php
+++ checkout.php
@@ -50,11 +50,34 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 									<input type="submit" name="submit" value="Buy Now" class="btn btn-success">
 								</form> -->
 							</div>
+
+						</div>
+					</div>
+					<div class="col-md-6">
+						<h3>Pay With</h3> 
+						<ul class="list-group">
+							<li class="list-group-item">
+								<form action="https://uat.esewa.com.np/epay/main" method="POST">
+									<input value="100" name="tAmt" type="hidden">
+									<input value="90" name="amt" type="hidden">
+									<input value="5" name="txAmt" type="hidden">
+									<input value="2" name="psc" type="hidden">
+									<input value="3" name="pdc" type="hidden">
+									<input value="epay_payment" name="scd" type="hidden">
+									<input value="ee2c3ca1-696b-4cc5-a6be-2c40d929d453" name="pid" type="hidden">
+									<input value="http://merchant.com.np/page/esewa_payment_success?q=su" type="hidden" name="su">
+									<input value="http://merchant.com.np/page/esewa_payment_failed?q=fu" type="hidden" name="fu">
+									<input type="image" src="image/esewa.png" name=""></li>
+								</form>
+								<li class="list-group-item"><input type="image" src="image/fonepay.png" name=""></li>
+								<li class="list-group-item"><input type="image" src="image/khalti.png" name=""></li>
+								<li class="list-group-item"><input type="image" src="image/hbl.jpeg" name=""></li>
+								
+							</ul>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</body>
-</html>
+	</body>
+	</html>
