ac2eff75353f07cad7544c7837497e278d49eb48
Bishnupkl <bishnup212@gmail.com>
2020-09-07 13:25:09 +0545

    showing some product list

--- dbconfig.php
+++ dbconfig.php
@@ -5,7 +5,7 @@
  $user='20nu';
  $pass='20nu';
 
- $conn=new mysqli($host,$db,$user,$pass);
+ $conn=new mysqli($host,$user,$pass,$db);
  if($conn->connect_error){
  	echo "failed to connect mySQL".$conn->connect_error;
  	exit;
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php include 'dbconfig.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ESEWA Payment Gateway</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="product-wrapper">
+			<div class="card" style="width: 18rem;">
+				<img src="..." class="card-img-top" alt="...">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
