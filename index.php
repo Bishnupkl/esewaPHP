9da0e3c54f3f6270c4c992f123ca5ad4ca29cebc
Bishnupkl <bishnup212@gmail.com>
2020-09-07 16:10:00 +0545

    adding image

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
-<?php include 'dbconfig.php'; ?>
+<?php include 'dbconfig.php'; 
+$sql="SELECT * FROM products";
+$result=mysqli_query($conn,$sql);
+// var_dump($result);
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,13 +12,22 @@
 </head>
 <body>
 	<div class="container">
-		<div class="product-wrapper">
-			<div class="card" style="width: 18rem;">
-				<img src="..." class="card-img-top" alt="...">
-				<div class="card-body">
-					<h5 class="card-title">Card title</h5>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-					<a href="#" class="btn btn-primary">Go somewhere</a>
+		<div class="pt-md-5">
+			<div class="row">	
+				<div class="product-wrapper">
+					<?php while ($products=mysqli_fetch_assoc($result)){ 
+						?>
+						<div class="col-md-4">
+							<div class="card" style="width: 18rem;">
+								<img src="image/<?php echo $products['image'] ?>" class="card-img-top" alt="...">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+									<a href="#" class="btn btn-primary">Go somewhere</a>
+								</div>
+							</div>
+						</div>
+					<?php } ?>
 				</div>
 			</div>
 		</div>
